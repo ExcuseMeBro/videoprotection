73a7e6e89a835881fa4770dc3823b0c0062706fc
OGABEK <shomoyxhacker007@mail.ru>
2025-07-15 23:14:57 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <iframe src="https://player.vdocipher.com/v2/?otp=20160313versASE323ZK3qezw7gQRWf7dstXArOAJlJiLwqwEQIcl0WJRpwuqsDF&playbackInfo=eyJ2aWRlb0lkIjoiOThkNjRhZWQzZGQ3ZjUzNzQ1ZDQ0MDdiMmVjNzQ3Y2IifQ==" allowFullScreen={true} style={{border: 0}} allow="encrypted-media"></iframe>
+    </>
+  )
+}
+
+export default App
