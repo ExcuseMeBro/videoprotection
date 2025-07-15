cf7146c9b84211d7ba0a08258790e8eb66bd5c8d
OGABEK <shomoyxhacker007@mail.ru>
2025-07-15 23:18:57 +0500

    fix: video

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <iframe src="https://player.vdocipher.com/v2/?otp=20160313versASE323ZK3qezw7gQRWf7dstXArOAJlJiLwqwEQIcl0WJRpwuqsDF&playbackInfo=eyJ2aWRlb0lkIjoiOThkNjRhZWQzZGQ3ZjUzNzQ1ZDQ0MDdiMmVjNzQ3Y2IifQ==" allowFullScreen={true} style={{border: 0}} allow="encrypted-media"></iframe>
+      <iframe src="https://player.vdocipher.com/v2/?otp=20160313versASE323ROc0Iya20QTEAPXO3gZw82ihHbRpJJpKFzOsMXStrEebYA&playbackInfo=eyJ2aWRlb0lkIjoiOThkNjRhZWQzZGQ3ZjUzNzQ1ZDQ0MDdiMmVjNzQ3Y2IifQ==" allowFullScreen={true} style={{border: 0}} allow="encrypted-media"></iframe>
     </>
   )
 }
