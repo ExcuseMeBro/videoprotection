b0b57aa8b36e00cd4a3a5b3275a98feb2e8ced66
OGABEK <shomoyxhacker007@mail.ru>
2025-07-15 23:20:35 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <iframe src="https://player.vdocipher.com/v2/?otp=20160313versASE323ROc0Iya20QTEAPXO3gZw82ihHbRpJJpKFzOsMXStrEebYA&playbackInfo=eyJ2aWRlb0lkIjoiOThkNjRhZWQzZGQ3ZjUzNzQ1ZDQ0MDdiMmVjNzQ3Y2IifQ==" allowFullScreen={true} style={{border: 0}} allow="encrypted-media"></iframe>
+      <iframe src="https://player.vdocipher.com/v2/?otp=20160313versASE323FP5VtWrcTqZoUkvwbGkpNSOcX3q9lmb7UnwOLjS53dlL3B&playbackInfo=eyJ2aWRlb0lkIjoiOThkNjRhZWQzZGQ3ZjUzNzQ1ZDQ0MDdiMmVjNzQ3Y2IifQ==" allowFullScreen={true} style={{border: 0}} allow="encrypted-media"></iframe>
     </>
   )
 }
